e37cd5a6b605cc13b75e12625255ac8667ec9a12
sbaadsgaard <sbaadsgaard@live.dk>
2023-08-09 16:11:30 +0200

    finish 4.6-7

--- part4/utils/list_helper.js
+++ part4/utils/list_helper.js
@@ -1,14 +1,66 @@
-// eslint-disable-next-line no-unused-vars
-const dummy = blogs => 1
+const _ = require("lodash")
 
-const totalLikes = blogs => blogs.reduce((sum, blog) => {
+const blogs = [
+    {
+        _id: "5a422a851b54a676234d17f7",
+        title: "React patterns",
+        author: "Michael Chan",
+        url: "https://reactpatterns.com/",
+        likes: 7,
+        __v: 0
+    },
+    {
+        _id: "5a422aa71b54a676234d17f8",
+        title: "Go To Statement Considered Harmful",
+        author: "Edsger W. Dijkstra",
+        url: "http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html",
+        likes: 5,
+        __v: 0
+    },
+    {
+        _id: "5a422b3a1b54a676234d17f9",
+        title: "Canonical string reduction",
+        author: "Edsger W. Dijkstra",
+        url: "http://www.cs.utexas.edu/~EWD/transcriptions/EWD08xx/EWD808.html",
+        likes: 12,
+        __v: 0
+    },
+    {
+        _id: "5a422b891b54a676234d17fa",
+        title: "First class tests",
+        author: "Robert C. Martin",
+        url: "http://blog.cleancoder.com/uncle-bob/2017/05/05/TestDefinitions.htmll",
+        likes: 10,
+        __v: 0
+    },
+    {
+        _id: "5a422ba71b54a676234d17fb",
+        title: "TDD harms architecture",
+        author: "Robert C. Martin",
+        url: "http://blog.cleancoder.com/uncle-bob/2017/03/03/TDD-Harms-Architecture.html",
+        likes: 0,
+        __v: 0
+    },
+    {
+        _id: "5a422bc61b54a676234d17fc",
+        title: "Type wars",
+        author: "Robert C. Martin",
+        url: "http://blog.cleancoder.com/uncle-bob/2016/05/01/TypeWars.html",
+        likes: 2,
+        __v: 0
+    }
+]
+// eslint-disable-next-line no-unused-vars
+const dummy = blogsList => 1
+
+const totalLikes = blogsList => blogsList.reduce((sum, blog) => {
     return sum + blog.likes
 }, 0)
 
-const favouriteBlog = blogs => {
-    return blogs.length === 0
+const favouriteBlog = blogsList => {
+    return blogsList.length === 0
         ? null
-        : blogs.reduce((currMax, blog) => {
+        : blogsList.reduce((currMax, blog) => {
             return blog.likes > currMax.likes
                 ? blog
                 : currMax
@@ -16,10 +68,33 @@ const favouriteBlog = blogs => {
 }
 
 
-console.log(favouriteBlog([]))
+const mostBlogs = (blogsList) => {
+    if (blogsList.length === 0) return null
+
+    const [author, blogs] = _.maxBy(
+        _.entries(
+            _.countBy(blogsList, "author")),
+        ([, count]) => count)
+    return { author, blogs }
+}
+
+
+
+const mostLikes = blogsList => {
+    if (blogsList.length === 0) return null
+    const groups = _.groupBy(blogsList, "author")
+    const likeCounts = _.map(_.entries(groups), ([author, blogs]) => {
+        return { author, likes: _.sumBy(blogs, "likes") }
+    })
+    return _.maxBy(likeCounts, "likes")
+}
+
+mostLikes(blogs)
 
 module.exports = {
     dummy,
     totalLikes,
-    favouriteBlog
+    favouriteBlog,
+    mostBlogs,
+    mostLikes
 }
