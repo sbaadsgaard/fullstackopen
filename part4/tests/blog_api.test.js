880c3d90852599b4bc9ee5b3a001e3b7071d7f57
sbaadsgaard <sbaadsgaard@live.dk>
2023-08-14 16:34:14 +0200

    finish 4.10

--- part4/tests/blog_api.test.js
+++ part4/tests/blog_api.test.js
@@ -1,14 +1,15 @@
 const mongoose = require("mongoose")
 const supertest = require("supertest")
+const _ = require("lodash")
 const app = require("../app")
-const helper = require("../utils/list_helper")
+const helper = require("./test_helper")
 const api = supertest(app)
 
 const Blog = require("../models/blog")
 
 beforeEach(async () => {
     await Blog.deleteMany({})
-    await Blog.insertMany(helper.initialBlogs())
+    await Blog.insertMany(helper.initialBlogs)
 })
 
 test("Blogs are returned in JSON format", async () => {
@@ -31,6 +32,28 @@ test("The unique identifier of a blog is named id", async () => {
 test("the returned blog should not have a __v property", async () => {
     const testBlog = (await api.get("/api/blogs")).body[0]
     expect(testBlog.__v).not.toBeDefined()
+})
+
+test("creating a POST request to /api/blogs/ successfully creates new blogpost", async () => {
+    const testBlogObject = {
+        title: "Musings about things",
+        author: "Mr. Anderson",
+        url: "www.fourohfour.com",
+        likes: 0
+    }
+
+    const blogsBefore = await helper.blogsInDb()
+    await api
+        .post("/api/blogs")
+        .send(testBlogObject)
+        .expect(201)
+
+    const blogsAfter = await helper.blogsInDb()
+    expect(blogsAfter.length).toBe(blogsBefore.length + 1)
+
+    const blogsWithoutID = blogsAfter.map(b => _.omit(b, "id"))
+    expect(blogsWithoutID).toContainEqual(testBlogObject)
+
 })
 
 
