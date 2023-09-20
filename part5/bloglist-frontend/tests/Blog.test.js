9edd9f2b5f74bb113f03c9834e34a79555a4fa2c
sbaadsgaard <sbaadsgaard@live.dk>
2023-09-20 19:04:48 +0200

    finish 5.14

--- part5/bloglist-frontend/tests/Blog.test.js
+++ part5/bloglist-frontend/tests/Blog.test.js
@@ -2,6 +2,8 @@ import React from 'react'
 import '@testing-library/jest-dom'
 import { render, screen } from '@testing-library/react'
 import Blog from '../src/components/Blog'
+import userEvent from '@testing-library/user-event'
+
 
 describe('Blog', () => {
     test('blog renders title and author, not url and likes', () => {
@@ -9,15 +11,36 @@ describe('Blog', () => {
             author: 'god',
             title: 'the bible',
             likes: 666,
-            url: 'heaven.org',
-            user: null
+            url: 'heaven.org'
         }
 
         const { container } = render(<Blog blog={blog}/>)
         screen.debug()
         const div = container.querySelector('.blogEntry')
         expect(div).toHaveTextContent('the bible god')
+        expect(div).not.toHaveTextContent('likes: 666')
+        expect(div).not.toHaveTextContent('url: heaven.org')
+    })
+
+    test('blog renders title and author when details button is clicked', async () => {
+        const blog = {
+            author: 'god',
+            title: 'the bible',
+            likes: 666,
+            url: 'heaven.org',
+            user: {
+                name: 'Jørgen'
+            }
+        }
+        const user = userEvent.setup()
+        const { container } = render(<Blog blog={blog} />)
+        const div = container.querySelector('.blogEntry')
+        const viewButton = container.querySelector('.viewButton')
         expect(div).not.toHaveTextContent('666')
         expect(div).not.toHaveTextContent('heaven.org')
+
+        await user.click(viewButton)
+        expect(div).toHaveTextContent('likes: 666')
+        expect(div).toHaveTextContent('url: heaven.org')
     })
 })
